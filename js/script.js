b151a85cedd1b83ed198cdb0ebff6058217889d5
Danil Butakov <12021109359@irgups.ru>
2022-04-04 02:30:45 +0800

    Сверстаны все странички сайта)

--- js/script.js
+++ js/script.js
@@ -57,6 +57,12 @@ $(document).ready(function() {
       $('.headline2__btn-next').on('click', function() {
         $('.short__slider2').slick('slickNext');
       });
+      $('.headline3__btn-prev').on('click', function() {
+        $('.description__slider3').slick('slickPrev');
+      });
+      $('.headline3__btn-next').on('click', function() {
+        $('.description__slider3').slick('slickNext');
+      });
       $('.short__slider2').slick({
         infinite: false,
         slidesToShow: 3,
@@ -77,6 +83,44 @@ $(document).ready(function() {
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+    });
+    $('.description__slider3').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 700,
+        arrows: false,
+        
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
               }
             },
             {

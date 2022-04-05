d8235872fca27d04c72dbaff1530f8613d6f5f53
Danil Butakov <12021109359@irgups.ru>
2022-04-06 01:09:24 +0800

    Сделал валидацию на главной странице

---
+++ sendmail.php
@@ -0,0 +1,35 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', 'phpmailer/language');
+    $mail->IsHtml(true);
+
+    $mail->setFrom(['email'] 'Dan');
+    $mail->addAddress('ibutakov52@gmail.com');
+    $mail->Subject = 'Привет! Это "Я"';
+
+    $body = '<h1>Это письмо!</h1>';
+
+    if(trim(!empty($_POST['email']))) {
+        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+    }
+
+    $mail->Body = $body;
+
+    if (!$mail->send()) {
+        $message = 'Ошибка';
+    } else {
+        $message = 'Данные отправлены!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>

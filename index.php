e30311d955c305bbc3c00200c00d66f73922a5e5
Danil Butakov <12021109359@irgups.ru>
2022-04-10 13:09:48 +0800

    Сделал как надо)

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'dan', '', 'anime-blog');
+$mysqli = new mysqli('localhost', 'root', 'admin', 'anime-blog');
 
     if(mysqli_connect_errno()) {
         printf('Соединение не установлено', mysqli_error());
